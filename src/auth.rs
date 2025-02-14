bcd3bd192bf6c15675b99786fdfe03ba1715692f
Matt Williams <matt.williams@bristol.ac.uk>
2025-02-14 16:26:30 +0000

    Update dependencies

--- src/auth.rs
+++ src/auth.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: MIT
 
 use anyhow::{Context, Result};
-use oauth2::reqwest::http_client;
 use oauth2::{
     basic::BasicClient, AuthType, AuthUrl, ClientId, DeviceAuthorizationUrl, Scope,
     StandardDeviceAuthorizationResponse, TokenUrl,
@@ -13,9 +12,21 @@ use url::Url;
 
 use crate::cache;
 
-pub fn client(client_id: &String, issuer_url: &Url) -> Result<BasicClient> {
+/// Given an OAuth `client_id` and URL, authenticate with the device code workflow
+pub fn get_access_token<P: AsRef<std::path::Path>>(
+    client_id: &String,
+    issuer_url: &Url,
+    open_webpage: bool,
+    show_qr: bool,
+    token_cache_path: P,
+) -> Result<AccessToken> {
+    // let http_client = reqwest::blocking::Client::new();
+    let http_client = oauth2::reqwest::blocking::ClientBuilder::new()
+        .redirect(reqwest::redirect::Policy::none())
+        .build()
+        .context("Could not build HTTP client for authorisation.")?;
+
     let client_id = ClientId::new(client_id.to_string());
-    let client_secret = None;
 
     // TODO get these from https://{provider}/realms/{realm}/.well-known/openid-configuration
     let auth_url =
@@ -26,28 +37,17 @@ pub fn client(client_id: &String, issuer_url: &Url) -> Result<BasicClient> {
         format!("{issuer_url}/protocol/openid-connect/auth/device").parse()?,
     );
     // Set up the config for the OIDC process.
-    let device_client = BasicClient::new(client_id, client_secret, auth_url, Some(token_url))
+    let device_client = BasicClient::new(client_id)
+        .set_auth_uri(auth_url)
+        .set_token_uri(token_url)
         .set_device_authorization_url(device_auth_url)
         .set_auth_type(AuthType::RequestBody);
 
-    Ok(device_client)
-}
-
-/// Given an OAuth `client_id` and URL, authenticate with the device code workflow
-pub fn get_access_token<P: AsRef<std::path::Path>>(
-    client_id: &String,
-    issuer_url: &Url,
-    open_webpage: bool,
-    show_qr: bool,
-    token_cache_path: P,
-) -> Result<AccessToken> {
-    let device_client = client(client_id, issuer_url)?;
     // Request the set of codes from the Device Authorization endpoint.
     let details: StandardDeviceAuthorizationResponse = device_client
         .exchange_device_code()
-        .context("Cound not exchange device code.")?
         .add_scope(Scope::new("openid".to_string()))
-        .request(http_client)
+        .request(&http_client)
         .context("Failed to request codes from device auth endpoint.")?;
 
     // Display the URL and user-code.
@@ -73,7 +73,7 @@ pub fn get_access_token<P: AsRef<std::path::Path>>(
     // Now poll for the token
     let token = device_client
         .exchange_device_access_token(&details)
-        .request(http_client, std::thread::sleep, None)
+        .request(&http_client, std::thread::sleep, None)
         .context("Could not get token from identity provider.")?;
 
     cache::write_file(token_cache_path, token.access_token().secret())?;
